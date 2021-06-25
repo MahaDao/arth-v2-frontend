a3c6ffee565e987e58966007e874413a443d42d2
mithil-jain <mithiljain8@gmail.com>
2021-06-25 13:47:49 +0530

    mixpanel fixed

--- src/analytics/Mixpanel.js
+++ src/analytics/Mixpanel.js
@@ -3,12 +3,12 @@ import mixpanel from 'mixpanel-browser';
 let key = '';
 if (process.env.NODE_ENV === "development") {
   key = '84c19ce0c48dff4b147960872628919a'
+} else if (window.location.hostname === 'arthcoin.com') {
+  key = '2742f0a859f9fd16638c1e86906497a2'
 } else if (process.env.REACT_APP_NETWORK === "matic") {
   key = 'b646039d2dd5e1e09deebb592cd041b1'
 } else if (process.env.REACT_APP_NETWORK === "ethereum") {
   key = '66a957f18e2f2cb7cbb235d8bb6823b1'
-} else {
-  key = '2742f0a859f9fd16638c1e86906497a2'
 }
 
 mixpanel.init(key);
