fae93e32d66f3aed59cde7356bf77e2f290d85bf
Yash Agrawal <yashagrawal18@outlook.com>
2021-06-25 20:32:09 +0530

    Minor fixes

--- src/views/Farming/components/ClaimModal.tsx
+++ src/views/Farming/components/ClaimModal.tsx
@@ -68,14 +68,6 @@ export default (props: IProps) => {
       <>
         <TransparentInfoDiv
           labelData={`You will receive`}
-          rightLabelUnit={'ARTHX'}
-          rightLabelValue={
-            Number(initEarnedARTHX)
-              .toLocaleString('en-US', { maximumFractionDigits: 6 })
-          }
-        />
-        <TransparentInfoDiv
-          labelData={`You will receive`}
           rightLabelUnit={'MAHA'}
           rightLabelValue={
             Number(initEarnedMAHA)
