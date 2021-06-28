73c92e6601ad60ba36788e903603055017bd57c6
mithil-jain <mithiljain8@gmail.com>
2021-06-28 17:53:31 +0530

    lock deposit ui and minor integration

--- src/views/Stats/components/StakeBox.tsx
+++ src/views/Stats/components/StakeBox.tsx
@@ -39,25 +39,8 @@ const CustomInfoCard = styled.div`
   }
 `;
 
-const CustomInfoCardHeader = styled.p`
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 300;
-  font-size: 16px;
-  line-height: 150%;
-  color: #ffffff;
-  margin-bottom: 24px;
-`;
-
 const CustomInfoCardDetails = styled.div`
   margin: 10px 0;
-`;
-
-const OneLine = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: baseline;
-  justify-content: flex-start;
 `;
 
 const TextWithIcon = styled.div`
@@ -69,31 +52,6 @@ const TextWithIcon = styled.div`
   color: rgba(255, 255, 255, 0.88);
   opacity: 0.64;
   vertical-align: center;
-`;
-
-const BeforeChip = styled.span`
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 24px;
-  color: #ffffff;
-`;
-
-const TagChips = styled.div`
-  background: rgba(255, 255, 255, 0.08);
-  border-radius: 4px;
-  padding: 2px 8px;
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 300;
-  font-size: 12px;
-  color: rgba(255, 255, 255, 0.64);
-`;
-
-const ToolTipFont = styled.p`
-  padding: 0px;
-  margin: 0px;
 `;
 
 export default StakeBox;
