2cd09dde4b02d5a5761ed28afe157f0e832afe47
mithil-jain <mithiljain8@gmail.com>
2021-06-30 15:00:51 +0530

    cleaned mint modal

--- src/views/Mint/components/MintModal.tsx
+++ src/views/Mint/components/MintModal.tsx
@@ -1,28 +1,12 @@
-import styled from 'styled-components';
-import { useWallet } from 'use-wallet';
-import {
-  Checkbox,
-  CheckboxProps,
-  createStyles,
-  Divider,
-  FormControlLabel,
-  makeStyles,
-  Slider,
-  Theme,
-  withStyles,
-} from '@material-ui/core';
+import { Divider, } from '@material-ui/core';
 import { parseUnits } from 'ethers/lib/utils';
-import CheckIcon from '@material-ui/icons/Check';
 import { BigNumber } from '@ethersproject/bignumber';
 import React, { useEffect, useMemo, useState } from 'react';
-import { withSnackbar, WithSnackbarProps } from 'notistack';
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 
 import TransparentInfoDiv from './InfoDiv';
 import useCore from '../../../hooks/useCore';
 import Button from '../../../components/Button';
 import CustomModal from '../../../components/CustomModal';
-import { CustomSnack } from '../../../components/SnackBar';
 import useTokenDecimals from '../../../hooks/useTokenDecimals';
 import { getDisplayBalance } from '../../../utils/formatBalance';
 import useMintARTH from '../../../hooks/callbacks/pools/useMintARTH';
@@ -42,7 +26,7 @@ interface IProps {
   onSuccess: () => void;
 }
 
-const MintModal = (props: WithSnackbarProps & IProps) => {
+const MintModal = (props: IProps) => {
   const {
     isInputFieldError,
     openModal,
@@ -149,15 +133,6 @@ const MintModal = (props: WithSnackbarProps & IProps) => {
                 size={'lg'}
                 onClick={() => {
                   onClose();
-                  let options = {
-                    content: () =>
-                      CustomSnack({
-                        onClose: props.closeSnackbar,
-                        type: 'red',
-                        data1: `Minting ${Number(collateralValue).toLocaleString()} ARTH cancelled`,
-                      }),
-                  };
-                  props.enqueueSnackbar('timepass', options);
                 }}
               />
             </div>
@@ -195,75 +170,18 @@ const MintModal = (props: WithSnackbarProps & IProps) => {
   );
 };
 
-const StakingDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 5px 0 0 0;
-`;
 
-const OneLineInput = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: baseline;
-  justify-content: flex-start;
-  margin: 5px 0 10px 0;
-`;
 
-const InputLabel = styled.p`
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 14px;
-  color: rgba(255, 255, 255, 0.64);
-  margin: 0px;
-`;
 
-const InternalSpan = styled.span`
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 12px;
-  line-height: 150%;
-  letter-spacing: 0.08em;
-  text-transform: uppercase;
-  color: #ffffff;
-`;
 
-const InputNoDisplay = styled.span`
-  background: rgba(255, 255, 255, 0.08);
-  border-radius: 4px;
-  padding: 2px 10px;
-  height: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0px 0px 0px 8px;
-`;
 
-const TimeSpan = styled.div`
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 300;
-  font-size: 12px;
-  line-height: 130%;
-  color: rgba(255, 255, 255, 0.88);
-`;
 
-const CheckboxDiv = styled.div`
-  background: rgba(255, 255, 255, 0.08);
-  border-radius: 6px;
-  padding: 5px 0px 0px 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 14px;
-  line-height: 20px;
-  text-align: center;
-  color: rgba(255, 255, 255, 0.88);
-  margin: 15px 0px 0px 0px;
-`;
 
-export default withSnackbar(MintModal);
+
+
+
+
+
+
+
+export default MintModal;
