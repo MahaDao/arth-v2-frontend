a3e699080ff45822b327acc9bd2014bf19548b18
Steven Enamakel <enamakel@vezures.com>
2021-07-02 15:06:59 +0300

    updated pool info

--- src/views/Mint/components/PoolInfo.tsx
+++ src/views/Mint/components/PoolInfo.tsx
@@ -24,12 +24,23 @@ interface IProps {
 export default ({ selectedCollateralCoin }: IProps) => {
   const { isLoading: isGlobalCollateraLoading, value: cr } = useGlobalCollateralRatio();
   const { isLoading: isARTHXOracleLoading, value: arthxPrice } = useARTHXOraclePrice();
-  const { isLoading: isCollateralPoolLoading, value: poolBalance } = useCollateralPoolBalance(selectedCollateralCoin);
-  const { isLoading: isPoolMintingLoading, value: mintingFee } = usePoolMintingFees(selectedCollateralCoin);
-  const { isLoading: isPoolRedeemFeesLoading, value: redeemingFee } = usePoolRedeemFees(selectedCollateralCoin);
+  const { isLoading: isCollateralPoolLoading, value: poolBalance } = useCollateralPoolBalance(
+    selectedCollateralCoin,
+  );
+  const { isLoading: isPoolMintingLoading, value: mintingFee } = usePoolMintingFees(
+    selectedCollateralCoin,
+  );
+  const { isLoading: isPoolRedeemFeesLoading, value: redeemingFee } = usePoolRedeemFees(
+    selectedCollateralCoin,
+  );
   const { isLoading: isStabilityFeeLoading, value: stabilityFee } = useStabilityFee();
-  const { isLoading: isCollateralPoolPriceLoading, value: collatearlPrice } = useCollateralPoolPrice(selectedCollateralCoin);
-  const { isLoading: isAvailableToMintLoading, value: availableToMint } = useAvailableToMint(selectedCollateralCoin);
+  const {
+    isLoading: isCollateralPoolPriceLoading,
+    value: collatearlPrice,
+  } = useCollateralPoolPrice(selectedCollateralCoin);
+  const { isLoading: isAvailableToMintLoading, value: availableToMint } = useAvailableToMint(
+    selectedCollateralCoin,
+  );
 
   return (
     <>
@@ -37,29 +48,34 @@ export default ({ selectedCollateralCoin }: IProps) => {
         <div style={{ marginBottom: '12px' }}>
           <OneLineInput>
             <div style={{ flex: 1 }}>
-              <TextForInfoTitle>ARTHX GMU Price</TextForInfoTitle>
+              <TextForInfoTitle>ARTHX Price</TextForInfoTitle>
             </div>
             <InputLabelSpanRight>
-              ${ 
-                isARTHXOracleLoading
-                  ? <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
-                  : Number(getDisplayBalance(arthxPrice, 6, 6))
-                    .toLocaleString('en-US', { maximumFractionDigits: 6 })
-              }
+              {isARTHXOracleLoading ? (
+                <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
+              ) : (
+                Number(getDisplayBalance(arthxPrice, 6, 6)).toLocaleString('en-US', {
+                  maximumFractionDigits: 6,
+                })
+              )}{' '}
+              ARTH
             </InputLabelSpanRight>
           </OneLineInput>
         </div>
         <div style={{ marginBottom: '12px' }}>
           <OneLineInput>
             <div style={{ flex: 1 }}>
-              <TextForInfoTitle>Collateral GMU Price</TextForInfoTitle>
+              <TextForInfoTitle>{selectedCollateralCoin} Price</TextForInfoTitle>
             </div>
             <InputLabelSpanRight>
-              ${ isCollateralPoolPriceLoading
-                ? <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
-                : Number(getDisplayBalance(collatearlPrice, 6, 6))
-                  .toLocaleString('en-US', { maximumFractionDigits: 6 })
-              }
+              {isCollateralPoolPriceLoading ? (
+                <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
+              ) : (
+                Number(getDisplayBalance(collatearlPrice, 6, 6)).toLocaleString('en-US', {
+                  maximumFractionDigits: 6,
+                })
+              )}{' '}
+              ARTH
             </InputLabelSpanRight>
           </OneLineInput>
         </div>
@@ -72,60 +88,68 @@ export default ({ selectedCollateralCoin }: IProps) => {
               </TextForInfoTitle>
             </div>
             <InputLabelSpanRight>
-              { isGlobalCollateraLoading
-                ? <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
-                : Number(getDisplayBalance(cr, 4, 4))
-                  .toLocaleString('en-US', { maximumFractionDigits: 4 })
-              }%
+              {isGlobalCollateraLoading ? (
+                <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
+              ) : (
+                Number(getDisplayBalance(cr, 4, 4)).toLocaleString('en-US', {
+                  maximumFractionDigits: 4,
+                })
+              )}
+              %
             </InputLabelSpanRight>
           </OneLineInput>
         </div>
         <div style={{ marginBottom: '12px' }}>
           <OneLineInput>
             <div style={{ flex: 1 }}>
-              <TextForInfoTitle>
-                Pool Balance
-              </TextForInfoTitle>
+              <TextForInfoTitle>Pool Balance</TextForInfoTitle>
             </div>
             <OneLineInput>
               <InputLabelSpanRight>
-                { isCollateralPoolLoading
-                  ? <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
-                  : prettyNumber(getDisplayBalance(poolBalance, 18))
-                }
+                {isCollateralPoolLoading ? (
+                  <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
+                ) : (
+                  prettyNumber(getDisplayBalance(poolBalance, 18))
+                )}
               </InputLabelSpanRight>
               <BeforeChip className="custom-mahadao-chip">{selectedCollateralCoin}</BeforeChip>
             </OneLineInput>
           </OneLineInput>
         </div>
         <div style={{ marginBottom: '12px' }}>
-              <OneLineInput>
-                <div style={{ flex: 1 }}>
-                  <TextForInfoTitle>Available to Mint</TextForInfoTitle>
-                </div>
-                <InputLabelSpanRight>
-                  { isAvailableToMintLoading
-                    ? <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
-                    : prettyNumber(getDisplayBalance(availableToMint, 18, 3))
-                  }
-                </InputLabelSpanRight>
-            <BeforeChip className="custom-mahadao-chip">ARTH</BeforeChip>
-              </OneLineInput>
+          <OneLineInput>
+            <div style={{ flex: 1 }}>
+              <TextForInfoTitle>Available to Mint</TextForInfoTitle>
             </div>
+            <InputLabelSpanRight>
+              {isAvailableToMintLoading ? (
+                <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
+              ) : (
+                prettyNumber(getDisplayBalance(availableToMint, 18, 3))
+              )}
+            </InputLabelSpanRight>
+            <BeforeChip className="custom-mahadao-chip">ARTH</BeforeChip>
+          </OneLineInput>
+        </div>
         <div style={{ marginBottom: '12px' }}>
           <OneLineInput>
             <div style={{ flex: 1 }}>
               <TextForInfoTitle>
                 Stability Fee
-                <CustomToolTip toolTipText={'Fee (charged in MAHA) associated with redeeming collateral.'} />
+                <CustomToolTip
+                  toolTipText={'Fee (charged in MAHA) associated with redeeming collateral.'}
+                />
               </TextForInfoTitle>
             </div>
             <InputLabelSpanRight>
-              { isStabilityFeeLoading
-                ? <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
-                : Number(getDisplayBalance(stabilityFee, 4, 4))
-                  .toLocaleString('en-US', { maximumFractionDigits: 4 })
-              }%
+              {isStabilityFeeLoading ? (
+                <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
+              ) : (
+                Number(getDisplayBalance(stabilityFee, 4, 4)).toLocaleString('en-US', {
+                  maximumFractionDigits: 4,
+                })
+              )}
+              %
             </InputLabelSpanRight>
           </OneLineInput>
         </div>
@@ -134,15 +158,20 @@ export default ({ selectedCollateralCoin }: IProps) => {
             <div style={{ flex: 1 }}>
               <TextForInfoTitle>
                 Minting Fee
-                <CustomToolTip toolTipText={'Fee (charged in ARTH) associated with minting new ARTH.'} />
+                <CustomToolTip
+                  toolTipText={'Fee (charged in ARTH) associated with minting new ARTH.'}
+                />
               </TextForInfoTitle>
             </div>
             <InputLabelSpanRight>
-              { isPoolMintingLoading
-                ? <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
-                : Number(getDisplayBalance(mintingFee, 4, 4))
-                  .toLocaleString('en-US', {maximumFractionDigits: 4})
-              }%
+              {isPoolMintingLoading ? (
+                <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
+              ) : (
+                Number(getDisplayBalance(mintingFee, 4, 4)).toLocaleString('en-US', {
+                  maximumFractionDigits: 4,
+                })
+              )}
+              %
             </InputLabelSpanRight>
           </OneLineInput>
         </div>
@@ -151,15 +180,22 @@ export default ({ selectedCollateralCoin }: IProps) => {
             <div style={{ flex: 1 }}>
               <TextForInfoTitle>
                 Redeeming Fee
-                <CustomToolTip toolTipText={'Fee (charged in respective collateral) associated with redeeming collateral.'} />
+                <CustomToolTip
+                  toolTipText={
+                    'Fee (charged in respective collateral) associated with redeeming collateral.'
+                  }
+                />
               </TextForInfoTitle>
             </div>
             <InputLabelSpanRight>
-              { isPoolRedeemFeesLoading
-                ? <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
-                : Number(getDisplayBalance(redeemingFee, 4, 4))
-                  .toLocaleString('en-US', { maximumFractionDigits: 4 })
-              }%
+              {isPoolRedeemFeesLoading ? (
+                <Loader color={'#ffffff'} loading={true} size={8} margin={2} />
+              ) : (
+                Number(getDisplayBalance(redeemingFee, 4, 4)).toLocaleString('en-US', {
+                  maximumFractionDigits: 4,
+                })
+              )}
+              %
             </InputLabelSpanRight>
           </OneLineInput>
         </div>
@@ -170,7 +206,12 @@ export default ({ selectedCollateralCoin }: IProps) => {
         </RightBottomCardTitle>
         <Grid container style={{ marginTop: '16px' }}>
           <Grid item lg={4}>
-            <Button text={'Earn Rewards'} size={'sm'} to={'/farming'} tracking_id={'earn_rewards_mintReedem'}/>
+            <Button
+              text={'Earn Rewards'}
+              size={'sm'}
+              to={'/farming'}
+              tracking_id={'earn_rewards_mintReedem'}
+            />
           </Grid>
         </Grid>
       </RightBottomCard>
@@ -224,7 +265,7 @@ const BeforeChip = styled.span`
   font-size: 16px;
   line-height: 24px;
   color: rgba(255, 255, 255, 0.64);
-`
+`;
 
 const RightBottomCard = styled.div`
   margin-top: 16px;
